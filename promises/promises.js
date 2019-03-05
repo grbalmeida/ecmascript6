78b068723e7378d2aad8502e32fad7857c8daabc
gilvan <g.r.almeida@live.com>
2019-03-04 22:21:48 -0300

    Promises

---
+++ promises/promises.js
@@ -0,0 +1,31 @@
+// Um promise possui 3 estados: não resolvido, resolvido, rejeitado
+
+let promise = new Promise((resolve, reject) => {
+    let result = true
+
+    if(result) {
+        resolve('Deu tudo certo 1!')
+    } else {
+        reject('Deu tudo errado 1!')
+    }
+})
+
+promise
+    .then(result => console.log(`Ok: ${result}`))
+    .catch(error => console.log(`Erro: ${error}`)) // Deu tudo certo 1!
+
+promise = new Promise((resolve, reject) => {
+    let result = false
+    let time = 2000
+    setTimeout(() => {
+        if(result) {
+            resolve('Deu tudo certo 2!')
+        } else {
+            reject('Deu tudo errado 2!');
+        }
+    })
+})
+
+promise
+    .then(result => console.log(`Ok: ${result}`))
+    .catch(error => `Erro: ${error}`) // Deu tudo certo 2!
